86bc0d6390d29edbc2f73f912bf5802e3120f86b
Alessandro Crinella <alecrinella99@gmail.com>
2022-11-01 16:45:18 +0100

    Add functionality to set query in url
    the query parameters is used by the API to filter recipes by name

--- src/components/aside/NameSearch.jsx
+++ src/components/aside/NameSearch.jsx
@@ -1,19 +1,35 @@
 import React from "react";
+import { useRef } from "react";
+import { useSearchParams } from "react-router-dom";
 
 import classes from "../../styles/NameSearch.module.css";
 
 export function NameSearch({}) {
-  /*
-    State for searching by name
-      useSearchParams 
-        On input set set params e.target
-          Return string up to filter recipes by name by context or state
-  */
+  const [searchParams, setSearchParams] = useSearchParams();
+  const queryRef = useRef();
+
+  const searchTerm = searchParams.get("query") || "";
+
+  const handleSearch = () => {
+    const query = queryRef.current.value;
+
+    if (query) {
+      setSearchParams({ query });
+    } else {
+      setSearchParams({});
+    }
+  };
 
   return (
     <div className={classes["search-section"]}>
       <h2>Search by name:</h2>
-      <input type="search" placeholder="Recipe name..." />
+      <input
+        ref={queryRef}
+        type="search"
+        placeholder="Recipe name..."
+        value={searchTerm}
+        onChange={handleSearch}
+      />
     </div>
   );
 }
