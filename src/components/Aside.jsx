6c0b6f03d3def912950cce84630c5ae8437ac509
Alessandro Crinella <alecrinella99@gmail.com>
2022-11-03 11:28:34 +0100

    Disable with overlay all filters when recipe detail is open

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -10,9 +10,17 @@ import classes from "../styles/Aside.module.css";
 
 import { X } from "react-feather";
 import { Menu } from "react-feather";
+import { useParams } from "react-router-dom";
 
 export function Aside() {
+  /*
+  When recipe detail is displayed all filter inputs must be disabled
+  Insted of declaring a state high on the tree, we check whether the url has a recipe id or not
+  */
+  const { id } = useParams();
+
   const [showAside, setShowAside] = useState(true);
+  const [disableModal, setDisableModal] = useState(false);
   const [activeTags, setActiveTags] = useState({
     cuisine: [],
     type: [],
@@ -35,6 +43,10 @@ export function Aside() {
     });
   };
 
+  const handleDisableModal = () => {
+    setDisableModal(true);
+  };
+
   const asideClasses = classes[`board__aside-${showAside ? "open" : "close"}`];
 
   const handleShowAside = () => {
@@ -54,9 +66,21 @@ export function Aside() {
         <>
           <X
             color="#333"
-            style={{ marginLeft: "auto", cursor: "pointer" }}
+            style={{ marginLeft: "auto", cursor: "pointer", zIndex: 1 }}
             onClick={handleShowAside}
           />
+          {id && (
+            <div
+              className={classes["disable-overlay"]}
+              onClick={handleDisableModal}
+            >
+              {disableModal && (
+                <div className={classes["disable-modal"]}>
+                  Close recipe detail to enable filters!
+                </div>
+              )}
+            </div>
+          )}
           <NameSearch />
           <Filters updateActiveFilters={handleUpdateActiveFilters} />
           <ActiveFilters activeFilters={activeTags} />
