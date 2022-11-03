1a49e18538852c7dab9eed0d87f8461151c2b3a0
Alessandro Crinella <alecrinella99@gmail.com>
2022-11-03 13:51:09 +0100

    Complete basic project

--- src/utils/recipes-fetcher.js
+++ src/utils/recipes-fetcher.js
@@ -2,7 +2,7 @@ const fetchRecipes = async (filtersString) => {
   const result = await fetch(
     `https://api.spoonacular.com/recipes/complexSearch${
       filtersString ? filtersString : "?"
-    }&diet=vegetarian&number=${process.env.KEY1}`
+    }&diet=vegetarian&number=100&apiKey=${import.meta.env.VITE_KEY_1}`
   );
   const data = await result.json();
   return data;
